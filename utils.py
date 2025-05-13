cbe3628af6209f027d8426df0b7ed3c61d0f7abf
Antoni Kirylczuk <antekkirylczuk@gmail.com>
2025-05-13 15:54:00 +0200

    Format code with Black

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 """Moduł z podstawowymi funkcjami matematycznymi do testowania."""
 
+
 def add(a: int, b: int) -> int:
     """Zwraca sumę dwóch liczb."""
     return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """Zwraca różnicę dwóch liczb."""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
     """Zwraca iloczyn dwóch liczb."""
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Zwraca iloraz dwóch liczb."""
